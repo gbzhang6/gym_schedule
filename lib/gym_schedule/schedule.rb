25f883e346c77e7b42db414565b7df0bdd076238
gbzhang6 <guibzhang@gmail.com>
2017-10-22 22:31:39 -0400

    starting the scraping process

--- lib/gym_schedule/schedule.rb
+++ lib/gym_schedule/schedule.rb
@@ -2,24 +2,15 @@ class GymSchedule::Schedule
   attr_accessor :name, :address, :class_time, :url
 
   def self.today
-    # puts <<-DOC.gsub /^\s*/, ''
-    # 1. UXF - 41st & 3rd - 6:30 AM - 7:30 AM
-    # 2. Advanced Barre - 41st & 3rd - 6:30 AM - 7:30 AM
-    # DOC
+    #scrape gym website and return the schedule for today based on the data
+    self.scrape_schedule
+  end
 
-    gymclass_1 = self.new
-    gymclass_1.name = "UXF"
-    gymclass_1.address = "41st & 3rd"
-    gymclass_1.class_time = "6:30 AM - 7:30 AM"
-    gymclass_1.url = "https://www.equinox.com/groupfitness/6495148"
+  def self.scrape_schedule
+    #go to the website and find the schedule for today
+    schedule = []
 
-    gymclass_2 = self.new
-    gymclass_2.name = "Advanced Barre"
-    gymclass_2.address = "41st & 3rd"
-    gymclass_2.class_time = "6:30 AM - 7:30 AM"
-    gymclass_2.url = "https://www.equinox.com/groupfitness/6443917"
-
-    [gymclass_1,gymclass_2]
+    schedule
   end
 
 end
