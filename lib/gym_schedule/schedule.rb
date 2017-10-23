1e16652aeba9189a30fec3e41594587804a78b30
gbzhang6 <guibzhang@gmail.com>
2017-10-22 23:03:15 -0400

    testing the equinox website scraping

--- lib/gym_schedule/schedule.rb
+++ lib/gym_schedule/schedule.rb
@@ -10,7 +10,14 @@ class GymSchedule::Schedule
     #go to the website and find the schedule for today
     schedule = []
 
+    schedule << self.scrape_equinox
+
     schedule
+  end
+
+  def self.scrape_equinox
+    doc = Nokogiri::HTML(open("https://www.equinox.com/classschedule?isBookingRequired=false&clubs=113,132,105,117,104,129,131,121,103,127,108,109,133,106,115,126,107,135,128,102,136,112,116,122,124,114,111,110"))
+    binding.pry
   end
 
 end
