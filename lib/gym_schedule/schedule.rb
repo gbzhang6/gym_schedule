ac6da516f787c07b3833baa69aeed5ef04fe2a1a
gbzhang6 <guibzhang@gmail.com>
2017-10-24 19:09:58 -0400

    continuing to refactor

--- lib/gym_schedule/schedule.rb
+++ lib/gym_schedule/schedule.rb
@@ -1,6 +1,8 @@
 class GymSchedule::Schedule
   attr_accessor :name, :address, :class_time, :url
 
+  @@all = []
+
   def self.today
     #scrape gym website and return the schedule for today based on the data
     self.scrape_schedule
@@ -10,18 +12,28 @@ class GymSchedule::Schedule
     #go to the website and find the schedule for today
     schedule = []
 
-    schedule << self.scrape_nysc
+    schedule << self.scrape_nysc_page
 
     schedule
   end
 
-  def self.scrape_nysc
+  def get_page
+    Nokogiri::HTML(open("https://www.newyorksportsclubs.com/classes?club=7th-avenue-at-10th-street+14th-5th-union-square+16th-8th+23rd-8th+23rd-park+36th-madison+38th-broadway+41st-3rd+41st-8th+48th-2nd+48th-6th-rockefeller-center+49th-broadway+51st-lexington+59th-park+62nd-broadway+63rd-west-end+73rd-central-park-west+76th-1st+76th-broadway+80th-broadway+91st-3rd+94th-broadway+115th-5th+125th-street-harlem-usa+145th-frederick-douglass-blvd+217-broadway-city-hall+astor-place+avenue-a+chelsea+fidi-30-broad-street+grand-central+irving-place+mercer-street+murray-hill+third-avenue+varick-street&day=10/23"))
+  end
+
+  def self.scrape_nysc_page
     self.new(
-      doc = Nokogiri::HTML(open("https://www.newyorksportsclubs.com/classes?club=7th-avenue-at-10th-street+14th-5th-union-square+16th-8th+23rd-8th+23rd-park+36th-madison+38th-broadway+41st-3rd+41st-8th+48th-2nd+48th-6th-rockefeller-center+49th-broadway+51st-lexington+59th-park+62nd-broadway+63rd-west-end+73rd-central-park-west+76th-1st+76th-broadway+80th-broadway+91st-3rd+94th-broadway+115th-5th+125th-street-harlem-usa+145th-frederick-douglass-blvd+217-broadway-city-hall+astor-place+avenue-a+chelsea+fidi-30-broad-street+grand-central+irving-place+mercer-street+murray-hill+third-avenue+varick-street&day=10/23")),
-      doc.css("a.bigger").text, #class_name
-      doc.css("span.address").text, #address
-      doc.css("span.big").text, #class_time
+      get_page.css("a.bigger").text, #class_name
+      get_page.css("span.address").text, #address
+      get_page.css("span.big").text, #class_time
     )
   end
+
+  def self.make_gymclass_list
+    get_page.each.with_index(1) do |schedule, i|
+      scrape_nysc_page
+    end
+  end
+
 
 end
