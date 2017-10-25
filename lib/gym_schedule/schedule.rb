1366459f15d1f60baa3e5404e817984670e7506b
gbzhang6 <guibzhang@gmail.com>
2017-10-24 21:03:04 -0400

    refactoring more

--- lib/gym_schedule/schedule.rb
+++ lib/gym_schedule/schedule.rb
@@ -15,13 +15,22 @@ class GymSchedule::Schedule
     schedule
   end
 
-  def self.scrape_nysc
+  def get_page
     doc = Nokogiri::HTML(open("https://www.newyorksportsclubs.com/classes?club=7th-avenue-at-10th-street+14th-5th-union-square+16th-8th+23rd-8th+23rd-park+36th-madison+38th-broadway+41st-3rd+41st-8th+48th-2nd+48th-6th-rockefeller-center+49th-broadway+51st-lexington+59th-park+62nd-broadway+63rd-west-end+73rd-central-park-west+76th-1st+76th-broadway+80th-broadway+91st-3rd+94th-broadway+115th-5th+125th-street-harlem-usa+145th-frederick-douglass-blvd+217-broadway-city-hall+astor-place+avenue-a+chelsea+fidi-30-broad-street+grand-central+irving-place+mercer-street+murray-hill+third-avenue+varick-street"))
+  end
 
-    gymclass = self.new
+  def scrape_gym_attributes
     gymclass.name = doc.css("div.bigger").text
     gymclass.address = doc.css("span.address").text
     gymclass.class_time = doc.css("span.big").text
   end
+
+  def self.scrape_nysc
+    scrape_gym_attributes.each do |g|
+      puts schedule
+    end
+  end
+
+
 
 end
