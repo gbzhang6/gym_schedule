cdd8297e3a40acb68f51169071f24f48675a521b
gbzhang6 <guibzhang@gmail.com>
2017-10-24 20:49:31 -0400

    continuing to refactor code so it's not iterating over each attribute

--- lib/gym_schedule/schedule.rb
+++ lib/gym_schedule/schedule.rb
@@ -19,22 +19,9 @@ class GymSchedule::Schedule
     doc = Nokogiri::HTML(open("https://www.newyorksportsclubs.com/classes?club=7th-avenue-at-10th-street+14th-5th-union-square+16th-8th+23rd-8th+23rd-park+36th-madison+38th-broadway+41st-3rd+41st-8th+48th-2nd+48th-6th-rockefeller-center+49th-broadway+51st-lexington+59th-park+62nd-broadway+63rd-west-end+73rd-central-park-west+76th-1st+76th-broadway+80th-broadway+91st-3rd+94th-broadway+115th-5th+125th-street-harlem-usa+145th-frederick-douglass-blvd+217-broadway-city-hall+astor-place+avenue-a+chelsea+fidi-30-broad-street+grand-central+irving-place+mercer-street+murray-hill+third-avenue+varick-street"))
 
     gymclass = self.new
-    gymclass.name = doc.css("div.bigger").each do |name|
-      puts name.text
-    end
-
-    gymclass.address = doc.css("span.address").each do |address|
-      puts address.text
-    end
-
-    gymclass.class_time = doc.css("span.big").each do |class_time|
-      puts class_time.text
-    end
-
-    gymclass.url = doc.css("#a.bigger href").each do |url|
-      puts url.text
-    end
-
+    gymclass.name = doc.css("div.bigger").text
+    gymclass.address = doc.css("span.address").text
+    gymclass.class_time = doc.css("span.big").text
   end
 
 end
