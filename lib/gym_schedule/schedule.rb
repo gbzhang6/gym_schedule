d528dc06818badf62c16f68e25823f80638686e7
gbzhang6 <guibzhang@gmail.com>
2017-10-27 15:40:21 -0400

    updated nokogiri scraper, using binding pry to look at data

--- lib/gym_schedule/schedule.rb
+++ lib/gym_schedule/schedule.rb
@@ -9,10 +9,10 @@ class GymSchedule::Schedule
 
   def self.scrape_gym_attributes
     doc = Nokogiri::HTML(open('https://www.newyorksportsclubs.com/classes?club=7th-avenue-at-10th-street+14th-5th-union-square+16th-8th+23rd-8th+23rd-park+36th-madison+38th-broadway+41st-3rd+41st-8th+48th-2nd+48th-6th-rockefeller-center+49th-broadway+51st-lexington+59th-park+62nd-broadway+63rd-west-end+73rd-central-park-west+76th-1st+76th-broadway+80th-broadway+91st-3rd+94th-broadway+115th-5th+125th-street-harlem-usa+145th-frederick-douglass-blvd+217-broadway-city-hall+astor-place+avenue-a+chelsea+fidi-30-broad-street+grand-central+irving-place+mercer-street+murray-hill+third-avenue+varick-street'))
-    name = doc.css("div.class-info p.padding icon-time span").text.strip
+    name = doc.css("div.cell.cell-md-left").text.strip.gsub("\n","")
     # address = doc.css("span.address").text.strip
     # gymclass_time = doc.css("span.big").text.strip
   end
-  binding.pry
+binding.pry
 
 end
